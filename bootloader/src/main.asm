798f9dbdee9803e37666dda2e8a0da28ed2dfcef
remxs2k <remosnotar@gmail.com>
2025-06-25 13:56:17 +0300

    added stack for string read

--- bootloader/src/main.asm
+++ bootloader/src/main.asm
@@ -1,17 +1,38 @@
 ORG 0x7C00 
 BITS 16
 
+start:
+    jmp main
+
+puts: 
+    push si 
+    push ax
+
+.loop:
+    lodsb
+    or al, al
+    jz .done
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
 main:
 
     mov ax, 0
     mov ds, ax
     mov es, ax 
 
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00
     
-    HLT
+    hlt
 
 .halt:
-    JMP .halt
+    jmp .halt
 
-times 510-($-$$) DB 0
-DW 0xAA55
+times 510-($-$$) db 0
+dw 0xAA55
