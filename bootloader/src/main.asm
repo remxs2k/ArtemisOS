b0f10cd5438e848110966d8deb4ee2be37ed34bf
remxs2k <remosnotar@gmail.com>
2025-06-25 14:16:34 +0300

    prints 'Hello World'

--- bootloader/src/main.asm
+++ bootloader/src/main.asm
@@ -1,6 +1,8 @@
 ORG 0x7C00 
 BITS 16
 
+%define ENDL 0x0D, 0x0A
+
 start:
     jmp main
 
@@ -12,6 +14,10 @@ puts:
     lodsb
     or al, al
     jz .done
+    mov ah, 0x0e ; call bios interrupt
+    mov bh, 0
+    int 0x10
+
     jmp .loop
 
 .done:
@@ -20,19 +26,24 @@ puts:
     ret
 
 main:
-
-    mov ax, 0
+    ; setup data segments
+    mov ax, 0 
     mov ds, ax
     mov es, ax 
 
     ; setup stack
     mov ss, ax
     mov sp, 0x7C00
+
+    mov si, msg_hello
+    call puts
     
     hlt
 
 .halt:
     jmp .halt
+
+msg_hello: 'Hello World!', ENDL, 0
 
 times 510-($-$$) db 0
 dw 0xAA55
