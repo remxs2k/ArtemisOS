b4413626d7122a603b60852058c45d23db98c14f
remxs2k <remosnotar@gmail.com>
2025-06-27 14:48:29 +0300

    Add boot sector memory handling and update main assembly code

--- bootloader/src/main.asm
+++ bootloader/src/main.asm
@@ -1,49 +1,18 @@
-ORG 0x7C00 
-BITS 16
+mov ah, 0x0e
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, '!'
+int 0x10
 
-%define ENDL 0x0D, 0x0A
+jmp $
 
-start:
-    jmp main
+times 510-($-$$) db 0    ; fills with 510 zeros minus the previous code
 
-puts: 
-    push si 
-    push ax
-
-.loop:
-    lodsb
-    or al, al
-    jz .done
-    mov ah, 0x0e ; call bios interrupt
-    mov bh, 0
-    int 0x10
-
-    jmp .loop
-
-.done:
-    pop ax
-    pop si
-    ret
-
-main:
-    ; setup data segments
-    mov ax, 0 
-    mov ds, ax
-    mov es, ax 
-
-    ; setup stack
-    mov ss, ax
-    mov sp, 0x7C00
-
-    mov si, msg_hello
-    call puts
-    
-    hlt
-
-.halt:
-    jmp .halt
-
-msg_hello: 'Hello World!', ENDL, 0
-
-times 510-($-$$) db 0
-dw 0xAA55
+dw 0xAA55 ; the 'magic' number
